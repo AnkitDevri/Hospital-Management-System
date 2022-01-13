b91e59f0413fbaf3d8311f7e5feba87864ab45b4
AnkitDevri <53830927+AnkitDevri@users.noreply.github.com>
2022-01-13 15:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask,url_for,redirect,render_template
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/',methods=['GET'])
 def welcome():
     return render_template('index.html')
 
